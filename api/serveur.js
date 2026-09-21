7b1bc1a8a6317fa2c0bd3f9a485a161ed790fca0
keliancombes <keliancombes@gmail.com>
2026-09-21 13:32:48 +0000

    chore: init API Express

---
+++ api/serveur.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+// Route de test : GET /
+app.get("/", (req, res) => {
+  res.json({ message: "Mon API fonctionne" });
+});
+let anime = [
+  { id: 1, nom: "db", episode: 639 },
+  { id: 2, nom: "jojo", episode: 190  },
+  { id: 3, nom: "naruto", episode: 720  }
+];
+
+// On demarre le serveur sur le port 3000
+app.listen(3000, () => {
+  console.log("Serveur sur http://localhost:3000");
+});
