6d0605bfebad4d301294264b01aca9e25bc039c2
keliancombes <keliancombes@gmail.com>
2026-09-21 13:39:49 +0000

    feat: donnees en memoire + route GET liste

--- api/serveur.js
+++ api/serveur.js
@@ -5,12 +5,15 @@ const app = express();
 app.get("/", (req, res) => {
   res.json({ message: "Mon API fonctionne" });
 });
-let anime = [
+let animes = [
   { id: 1, nom: "db", episode: 639 },
   { id: 2, nom: "jojo", episode: 190  },
   { id: 3, nom: "naruto", episode: 720  }
 ];
-
+// GET /animes -> renvoie tout le tableau
+app.get("/animes", (req, res) => {
+  res.json(animes);
+});
 // On demarre le serveur sur le port 3000
 app.listen(3000, () => {
   console.log("Serveur sur http://localhost:3000");
